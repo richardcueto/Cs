de281cf64e049e616a837f799560a4d8d1b88f74
richard Personal <richard.cueto@outlook.com.pe>
2024-11-10 14:03:28 -0500

    fin hasta nuevo futuro

--- miProyecto/Program.cs
+++ miProyecto/Program.cs
@@ -1,4 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hola:");
-string nombre=Console.ReadLine();
-Console.WriteLine(nombre);
+﻿using System;
+
+class Hello
+{
+    static void Main()
+    {
+        // This line prints "Hello, World" 
+        Console.WriteLine("Hello, World");
+    }
+}
